71c08d580853d1088e6a88861d3913c7f4b81187
Tom Maenhout <tom.maenhout@gmail.com>
2021-08-21 22:39:12 -0300

    testing

--- test/TomToken.test.js
+++ test/TomToken.test.js
@@ -1,18 +1,27 @@
-const TomToken = artifacts.require("TomToken");
+const Token = artifacts.require("MyToken");
+
 var chai = require("chai");
-const BN = web.utils.BN;
-const chaiBN = require ("chai-bn")(BN);
+
+const BN = web3.utils.BN;
+const chaiBN = require('chai-bn')(BN);
 chai.use(chaiBN);
 
-var chaiAsPromised = require ("chai-as-promised");
+var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
-contract ("Token Test", async (accounts) => {
-    it ("all tokens should be in my account", async => {
-            let instance = await TomToken.deployed();
-            let totalSupply = await instance.totalSupply();
-         
-       
-    })
-}
+
+contract("Token Test", async accounts => {
+    const [ initialHolder, recipient, anotherAccount ] = accounts;
+
+
+    it("All tokens should be in my account", async () => {
+    let instance = await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    //old style:
+    //let balance = await instance.balanceOf.call(initialHolder);
+    //assert.equal(balance.valueOf(), 0, "Account 1 has a balance");
+    //condensed, easier readable style:
+    await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+    });
+});
