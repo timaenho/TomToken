27a18576b8379fae025ccc30cfd43bc9e840f325
Tom Maenhout <tom.maenhout@gmail.com>
2021-08-30 19:14:56 -0300

    Unit-testing

--- test/TomToken.test.js
+++ test/TomToken.test.js
@@ -24,4 +24,21 @@ contract("Token Test", async accounts => {
     //condensed, easier readable style:
     await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
+
+    it("It's possible to send tokens between accounts", async() => {
+        const sendtokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        await expect (instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect (instance.transfer(recipient,sendtokens)).to.eventually.be.fulfilled;
+        await expect (instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendtokens)));
+        await expect (instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendtokens))
+    })
+
+    it("It's not possible to send more tokens than account 1 has", async () => {
+        let instance = await Token.deployed();
+        let balanceOfAccount = await instance.balanceOf(initialHolder);
+        await expect(instance.transfer(recipient, new BN(balanceOfAccount+1))).to.eventually.be.rejected;
+        await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balanceOfAccount);
+    })
 });
