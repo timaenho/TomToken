47c43bbba26e55e6c537a5f81462f3ab007bd6c3
timaenho <tom.maenhout@gmail.com>
2021-11-17 01:53:02 -0300

    new-proyect

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,5 @@
-var TomToken = artifacts.require("./TomToken.sol");
-var MyTokenSales = artifacts.require("./MyTokenSale.sol");
-var KycContract = artifacts.require("./KycContract.sol")
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
-
-module.exports = async function(deployer) {
-let addr = await web3.eth.getAccounts();
-await deployer.deploy(TomToken, 1000000000);
-await deployer.deploy(KycContract);
-await deployer.deploy(MyTokenSales, 1, addr[0], TomToken.address, KycContract.address);
-let tokenInstance = await TomToken.deployed();
-await tokenInstance.transfer(MyTokenSales.address, 1000000000);
+module.exports = function(deployer) {
+  deployer.deploy(SimpleStorage);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -5,15 +5,8 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    development: {
-      port: "7545",
-      network_id: "5777",
-      host: "127.0.0.1"
-    }
-  },
-  compilers: {
-    solc:{
-     version:"0.8.7" 
+    develop: {
+      port: 8545
     }
   }
 };
